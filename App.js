29516b2cc1b6e0fa3a5d49e2d8c773293f20b954
tmangowal <tmangowal@gmail.com>
2020-07-06 11:33:13 +0700

    app js

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={{ fontFamily: "AvenirNextLTPro_Medium" }}>
+      <Text style={{ fontFamily: "AvenirNextLTPro-Bold" }}>
         Open up App.js to start working on your app!
       </Text>
       <StatusBar style="auto" />
